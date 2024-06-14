72cd72ba4fbba4ab16d3dbeeddad4715c0d9ff73
omardwwm <boudraa.omar@gmail.com>
2024-06-14 23:14:19 +0200

    update YML TO GENERATE REPORT with failed test V3

--- tests/failedTest.spec.js
+++ tests/failedTest.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 const { HomePage } = require("../pages/homePage");
 
-test("GO HOME PAGE OK", async ({ page }) => {
+test("Test failed results", async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
   await homePage.failedTest();
